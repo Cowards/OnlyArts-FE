2206006e1ce1fd89fc5bdee4c8997e049cc6c53a
neru <ngocngocthuc@gmail.com>
2024-04-05 04:03:18 +0700

    style: checkout

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -4,8 +4,15 @@ loadAccountInfo();
 const loadCartInfo = async () => {
   const cartArtworks = await http.send("GET", "/api/v3/cart");
   let total = 0;
+  const billing = document.querySelector(".billing-details");
   cartArtworks.forEach(async (artwork) => {
     total += artwork.price;
+    billing.innerHTML += `
+      <div class="billing-item">
+        <p class="artwork-name">${artwork.name}</p>
+        <p class="artwork-price">$ ${artwork.price}</p>
+      </div>
+    `;
   });
   const totalCart = document.querySelector("#amount");
   totalCart.value = `${total}`;
@@ -27,7 +34,7 @@ const form = document.querySelector("#createOrder");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const data = new FormData(form);
-  const amount = data.get("amount") * 23000;
+  const amount = data.get("amount");
   const url = await send("POST", `/checkout/create_payment_url/${amount}`);
   console.log(url);
   window.location.href = url.vnpUrl;
